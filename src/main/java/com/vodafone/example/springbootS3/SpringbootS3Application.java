795ab5cb9e8fd8605ff026115f1f80d0199e3232
Adrian <38786314+elmonje@users.noreply.github.com>
2023-02-19 19:44:19 +0100

    Update SpringbootS3Application.java

--- src/main/java/com/vodafone/example/springbootS3/SpringbootS3Application.java
+++ src/main/java/com/vodafone/example/springbootS3/SpringbootS3Application.java
@@ -2,12 +2,19 @@ package com.vodafone.example.springbootS3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootS3Application {
+public class SpringbootS3Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootS3Application.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootS3Application.class);
+    }		
 
 }
